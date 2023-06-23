2d708a4a8537129f220b884cd4b2488d965309dc
kondrat <85776336+drugsurfer@users.noreply.github.com>
2023-06-23 20:57:32 +0300

    Add files via upload

--- db.py
+++ db.py
@@ -23,10 +23,10 @@ def init_user_db(force: bool = False):
 
     c.execute('''
         CREATE TABLE IF NOT EXISTS user_data (
-            id          INTEGER PRIMARY KEY,
-            user_id     INTEGER NOT NULL,
-            user_name   TEXT NOT NULL,
-            user_sur_name TEXT NOT NULL);
+            id_primary          INTEGER PRIMARY KEY,
+            id     INTEGER NOT NULL,
+            name   TEXT NOT NULL,
+            sur_name TEXT NOT NULL);
     ''')
 
     connection.commit()
@@ -34,7 +34,7 @@ def init_user_db(force: bool = False):
 def init_students_db(force: bool = False):
     '''
     инициализирует базу учеников. force может перезаписать старую
-    user_id - id преподавателя, который отвечает за ученика
+    user_id -> id FROM user_data
 
     '''
     connection = get_connection()
@@ -45,11 +45,12 @@ def init_students_db(force: bool = False):
 
     c.execute('''
         CREATE TABLE IF NOT EXISTS students (
-            id          INTEGER PRIMARY KEY,
-            user_id INTEGER NOT NULL,
+            id_primary          INTEGER PRIMARY KEY,
+            id INTEGER NOT NULL,
             name   TEXT NOT NULL,
             sur_name TEXT NOT NULL,
-            science_object TEXT NOT NULL);
+            science_object TEXT NOT NULL,
+            lesson_cost INTEGER NOT NULL);
     ''')
 
     connection.commit()
@@ -62,10 +63,10 @@ def add_user_to_db(user: tuple):
     connection = get_connection()
     c = connection.cursor()
     if check_id_in_db(user[0], 'user_data'):
-        print(user[0], 'Уже есть в базе') # такой user_id уже есть в БД
+        print(user[0], 'Уже есть в таблице users') # такой user_id уже есть в БД
     else:
         # такого user_id нет в БД
-        c.execute('INSERT INTO user_data (user_id, user_name, user_sur_name) VALUES (?, ?, ?);', (user[0], user[1], user[2]))
+        c.execute('INSERT INTO user_data (id, name, sur_name) VALUES (?, ?, ?);', (user[0], user[1], user[2]))
     connection.commit()
 
 def search_students_in_db(user_id: int, student_name: str, student_science: str):
@@ -75,19 +76,25 @@ def search_students_in_db(user_id: int, student_name: str, student_science: str)
     student_name - имя ученика
     student_science - предмет, которым занимается ученик
     '''
-    pass
+    connection = get_connection()
+    c = connection.cursor()
+    return c.execute('SELECT * FROM students WHERE id = ? AND name = ? AND science_object = ?', (user_id, student_name, student_science)).fetchall()
 
 def add_student_to_db(student: tuple):
     '''
     Добавляет ученика в students.
-    student = (student_id: int, user_id: int, name: str, sur_name: str, science_object: str)
+    student = (user_id: int, name: str, sur_name: str, science_object: str, lesson_cost: int)
     '''
-    student_id, user_id, name, sur_name, science_object = student
+    user_id, name, sur_name, science_object, lesson_cost = student
     connection = get_connection()
     c = connection.cursor()
     if check_id_in_db(user_id, 'user_data'):
         # преподаватель ученика есть в базе
-        pass
+        if len(search_students_in_db(user_id, name, science_object)) == 0:
+            c.execute('INSERT INTO students (id, name, sur_name, science_object, lesson_cost) VALUES (?, ?, ?, ?, ?);',
+            (user_id, name, sur_name, science_object, lesson_cost))
+        else:
+            print(name, 'уже есть в таблице students')
     else:
         # ну каким-то волшебным образом не будет преподавателя в базе
         pass
@@ -96,27 +103,52 @@ def add_student_to_db(student: tuple):
 def check_id_in_db(id: int, db_name: str):
     connection = get_connection()
     c = connection.cursor()
-    if db_name == 'user_data':
-        # КОСТЫЛЬ СДЕЛАТЬ ТАК ЧТОБЫ ВО ВСЕХ ТАБЛИЦА id НАЗЫВАЛСЯ ОДИНАКОВО
-        pass
+    if c.execute('SELECT EXISTS (SELECT 1 FROM {} WHERE id = ? LIMIT 1);'.format(db_name), (id, )).fetchone()[0]:
+        return True # такой id уже есть в БД
     else:
-        pass
-    if c.execute('SELECT EXISTS (SELECT 1 FROM {} WHERE user_id = ? LIMIT 1);'.format(db_name), (id, )).fetchone()[0]:
-        return True # такой user_id уже есть в БД
-    else:
-        # такого user_id нет в БД
+        # такого id нет в БД
         return False
+
+def check_table_in_db(table_name: str) -> bool:
+    '''
+    Проверяет наличие таблицы в базе
+    '''
+    connection = get_connection()
+    c = connection.cursor()
+    if c.execute('SELECT count(*) FROM sqlite_master WHERE type = "table" AND name = ?;', (table_name,)):
+        return True
+    else:
+        return False
+
+def replace_value(value, column_name: str, table_name: str, id: int):
+    '''
+    Заменяет переданное значение value в переданном столбце column_name
+    table_name - название таблицы
+    id - id записи (PRIMARY KEY only)
+    '''
+    connection = get_connection()
+    c = connection.cursor()
+    # Проверка наличия таблицы в БД
+    if check_table_in_db(table_name):
+        try:
+            c.execute('UPDATE {} SET {} = ? WHERE id_primary = {}'.format(table_name, column_name, id), (value, ))
+        except:
+            print('Не удалось обновить значение в таблице ', table_name)
+    else:
+        print('Не найдена таблица ', table_name)
+    connection.commit()
 
 def check_db():
     connection = get_connection()
     c = connection.cursor()
-    c.execute('SELECT * FROM user_data;')
+    c.execute('SELECT * FROM students;')
     print(c.fetchall())
 
+    
 init_user_db()
+init_students_db()
 check_db()
-add_user_to_db((43333000, 'Евгений', 'Кондратьев'))
-add_user_to_db((43333333400, 'Евгений', 'Кондратьев'))
-add_user_to_db((43333000, 'Евгений', 'Кондратьев'))
-add_user_to_db((3232323323, 'ЛОХ', 'ЛОХОВИЧ'))
+add_user_to_db((1001, 'Евгений', 'Кондратьев'))
+add_student_to_db((1001, 'Мария', 'Иванова', 'Физика', 700))
+replace_value('Кондратьев', 'sur_name', 'user_data', 1)
 check_db()
